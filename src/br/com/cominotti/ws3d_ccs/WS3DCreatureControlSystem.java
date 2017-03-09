631a0f62e19ee3b05b90d2f7caa0d874936a080d
Danilo Cominotti Marques <dcominottim@gmail.com>
2017-03-09 11:52:16 -0300

    update

--- src/br/com/cominotti/ws3d_ccs/WS3DCreatureControlSystem.java
+++ src/br/com/cominotti/ws3d_ccs/WS3DCreatureControlSystem.java
@@ -7,10 +7,12 @@ package br.com.cominotti.ws3d_ccs;
 
 import br.com.cominotti.ws3d_ccs.gui.creature_control.MoveForwardButton;
 import br.com.cominotti.ws3d_ccs.gui.creature_control.WorldRegistry;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -24,30 +26,35 @@ import ws3dproxy.model.Creature;
 import ws3dproxy.model.World;
 
 /**
- *
  * @author ia941
  */
 public class WS3DCreatureControlSystem extends Application {
-    
+
     private static Creature creature;
-    
+
     public static void main(String[] args) throws CommandExecException {
-               launch(args);
-         try {
+        try {
             Executors.newSingleThreadExecutor().submit(() -> {
-                   worldserver3d.Main.main(null);
+                worldserver3d.Main.main(null);
             });
-         
+
         } catch (Exception ex) {
             Logger.getLogger(WS3DCreatureControlSystem.class.getName()).log(Level.SEVERE, null, ex);
         }
-         
-       WorldRegistry worldRegistry = WorldRegistry.getInstance();
 
-         Logger.getLogger(WS3DCreatureControlSystem.class.getName()).log(Level.SEVERE, null, "hey");
-        
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        WorldRegistry worldRegistry = WorldRegistry.getInstance();
+
+        launch(args);
+        Logger.getLogger(WS3DCreatureControlSystem.class.getName()).log(Level.SEVERE, null, "hey");
+
     }
-    
+
     @Override
     public void start(Stage primaryStage) throws CommandExecException {
         primaryStage.setTitle("W3SD Creature Control System Panel");
@@ -60,8 +67,7 @@ public class WS3DCreatureControlSystem extends Application {
             }
         });
 
-    
-        
+
         StackPane root = new StackPane();
         root.getChildren().add(MoveForwardButton.createButton(WorldRegistry.getInstance()));
         primaryStage.setScene(new Scene(root, 300, 250));
